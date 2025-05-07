bf203477b7ff0779f85a6537a83cf456c7bf1a25
yjh1008 <71313511+yjh-1008@users.noreply.github.com>
2025-05-07 23:33:33 +0900

    feat: 주석

--- chapter2/evaluation.js
+++ chapter2/evaluation.js
@@ -1,29 +1,113 @@
-// type Evaluatable<A, B> = [(...args: A[]) => B, ...A[]];
-// function evaluation<A, B>(expr: Evaluatable<A, B>): B {
-//   const [fn, ...args] = expr;
-//   return fn(...args);
-// }
-// const add = (a: number, b: number) => a + b;
-// const result = evaluation([add, 1, 2]);
-// console.log(result); //3
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function fx(iterable) {
+    return new FxIterable(iterable);
+}
+function filter(f, iterable) {
+    var _i, iterable_1, a;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _i = 0, iterable_1 = iterable;
+                _a.label = 1;
+            case 1:
+                if (!(_i < iterable_1.length)) return [3 /*break*/, 4];
+                a = iterable_1[_i];
+                if (!f(a)) return [3 /*break*/, 3];
+                return [4 /*yield*/, a];
+            case 2:
+                _a.sent();
+                _a.label = 3;
+            case 3:
+                _i++;
+                return [3 /*break*/, 1];
+            case 4: return [2 /*return*/];
+        }
+    });
+}
+function map(f, iterable) {
+    var _i, iterable_2, i;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _i = 0, iterable_2 = iterable;
+                _a.label = 1;
+            case 1:
+                if (!(_i < iterable_2.length)) return [3 /*break*/, 4];
+                i = iterable_2[_i];
+                return [4 /*yield*/, f(i)];
+            case 2:
+                _a.sent();
+                _a.label = 3;
+            case 3:
+                _i++;
+                return [3 /*break*/, 1];
+            case 4: return [2 /*return*/];
+        }
+    });
+}
 var FxIterable = /** @class */ (function () {
     function FxIterable(iterable) {
         this.iterable = iterable;
     }
-    //이터레이터 객체 반환
     FxIterable.prototype[Symbol.iterator] = function () {
         return this.iterable[Symbol.iterator]();
     };
-    //filter 메서드 추가
     FxIterable.prototype.filter = function (fn) {
-        return new FxIterable(filter(fn, this.iterable));
+        return fx(filter(fn, this.iterable));
     };
     FxIterable.prototype.reject = function (fn) {
-        return this.filter(function (a) { return !fn(a); });
+        return fx(filter(function (a) { return !fn(a); }, this.iterable));
+    };
+    FxIterable.prototype.map = function (f) {
+        return fx(map(f, this.iterable));
+    };
+    FxIterable.prototype.to = function (converter) {
+        return converter(this);
+    };
+    FxIterable.prototype.chain = function (f) {
+        return fx(f(this));
+    };
+    FxIterable.prototype.toArray = function () {
+        return __spreadArray([], this.iterable, true);
     };
     return FxIterable;
 }());
-//JavaScript는 이 구조 분해 할당을 처리하기 위해 내부적으로 이터레이터를 사용.
-var fx = new FxIterable([1, 2, 3]);
-var iter = fx.filter(function (a) { return a % 2 === 0; });
-console.log(iter);
+var result = fx([1, 2, 3])
+    .filter(function (a) { return a % 2 === 0; })
+    .to(function (iterable) { return __spreadArray([], iterable, true); });
+console.log(result); // [2]
--- chapter2/evaluation.ts
+++ chapter2/evaluation.ts
@@ -1,20 +1,38 @@
-function filter(f: Function, iterable: Iterable<any>) {
-  const iterator = iterable[Symbol.iterator]();
-
-  return {
-    next() {
-      while (true) {
-        const { done, value } = iterator.next();
-        if (done) return { done, value };
-        if (f(value)) return { value };
-      }
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
+function fx<A>(iterable: Iterable<A>) {
+  return new FxIterable(iterable);
 }
+function* filter<A>(
+  f: (a: A) => boolean,
+  iterable: Iterable<A>
+): IterableIterator<A> {
+  for (const a of iterable) {
+    if (f(a)) {
+      yield a;
+    }
+  }
+}
+function* map(f: Function, iterable: Iterable<any>) {
+  for (const i of iterable) {
+    yield f(i);
+  }
+}
+/**
+ * this → 현재 FxIterable 인스턴스
 
+this.iterable → 우리가 생성자에서 받은 iterable 값 (Array, generator, etc.)
+
+this.iterable[Symbol.iterator]() → 원래 iterable의 이터레이터를 꺼내옴
+
+즉, FxIterable 인스턴스를 for..of로 순회하거나 ...로 풀면:
+
+JS는 FxIterable[Symbol.iterator]()를 호출
+
+이 메서드에서 this.iterable[Symbol.iterator]()를 호출
+
+실제로 안에 있던 데이터 (Array, generator)의 이터레이터가 동작
+
+
+ */
 class FxIterable<A> {
   constructor(private iterable: Iterable<A>) {}
   //이터레이터 객체 반환
@@ -24,14 +42,16 @@ class FxIterable<A> {
 
   //filter 메서드 추가
   filter(fn: (value: A) => boolean) {
-    return new FxIterable(filter(fn, this.iterable));
+    // console.log(this.iterable, "1", filter(fn, this.iterable).next());
+    return fx(filter(fn, this));
   }
   reject(fn: (value: A) => boolean): FxIterable<A> {
     return new FxIterable(filter((a: A) => !fn(a), this.iterable));
   }
 
   toArray() {
-    return [...this];
+    // console.log(this.iterable);
+    return [...this.iterable];
   }
 
   // this가 뭐야?
@@ -40,16 +60,28 @@ class FxIterable<A> {
   // 즉, this는 FxIterable 클래스의 인스턴스를 참조하는 키워드
   // 이 클래스의 인스턴스는 이터레이터 객체를 반환하는 [Symbol.iterator] 메서드를 가지고 있음
   // 이 메서드는 현재 객체를 반환하므로, this는 현재 객체를 참조하는 키워드
-  to<R>(converter: (iterable: this) => R): R {
+  to<R>(converter: (iterable: Iterable<A>) => R): R {
     return converter(this);
   }
   chain<B>(f: (iterable: this) => Iterable<B>): FxIterable<B> {
     return fx(f(this));
   }
+
+  map<B>(f: (value: A) => B): FxIterable<B> {
+    console.log(map(f, this.iterable));
+    return fx(map(f, this.iterable));
+  }
 }
+
+const sorted = fx([1, 2, 3])
+  .filter((a) => a % 2 === 0)
+  .to((iterable) => [...iterable]);
+
+// .map((a) => a + 1);
+// const [first, second] = ;
+console.log(sorted);
+// sorted.to((iterable) => [...iterable]);
 //JavaScript는 이 구조 분해 할당을 처리하기 위해 내부적으로 이터레이터를 사용.
-const fx = new FxIterable([1, 2, 3]);
+// const fx = new FxIterable([1, 2, 3]);
 
-const iter = fx.filter((a) => a % 2 === 0);
-
-console.log(iter);
+// const iter = fx.filter((a) => a % 2 === 0);
